08f84d127e8e3877ec0c9f509eb952e644e7cfc8
Tyler Wheeler <tyler0wheeler@gmail.com>
2020-12-09 15:40:54 -0500

    testing current user

--- blueprints/searchedrecipes.py
+++ blueprints/searchedrecipes.py
@@ -32,7 +32,7 @@ def create_recipe():
     print(payload["readyInMinutes"])
     print(payload["instructions"])
     print(payload["recipeId"])
-    print(payload["owner"])
+    print(current_user)
     return jsonify(data={}, status={"code": 200, "message": "Success"})
 
 # @searchedrecipe.route('/searchedingredient/<recipe_id>', methods=["POST"])
