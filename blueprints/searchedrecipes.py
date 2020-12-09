ff5c349b77c0d8c39b0b725dc3274d0cbb812150
Tyler Wheeler <tyler0wheeler@gmail.com>
2020-12-09 15:04:50 -0500

    test put route

--- blueprints/searchedrecipes.py
+++ blueprints/searchedrecipes.py
@@ -23,21 +23,21 @@ def get_one_user():
 def create_recipe():
     payload = request.get_json()
     print(type(payload), payload)
-    user_searched_recipe = models.SearchedRecipe.create(title=payload["title"], servings=payload["servings"], image=payload["image"], readyInMinutes=payload["readyInMinutes"], instructions=payload["instructions"], owner=current_user.id, recipeId=payload["recipeId"], ingredients=payload["ingredients"])
-    searched_recipe_dict = model_to_dict(user_searched_recipe)
-    return jsonify(data=searched_recipe_dict, status={"code": 200, "message": "Success"})
+    # user_searched_recipe = models.SearchedRecipe.create(title=payload["title"], servings=payload["servings"], image=payload["image"], readyInMinutes=payload["readyInMinutes"], instructions=payload["instructions"], owner=current_user.id, recipeId=payload["recipeId"], ingredients=payload["ingredients"])
+    # searched_recipe_dict = model_to_dict(user_searched_recipe)
+    return jsonify(data={}, status={"code": 200, "message": "Success"})
 
-@searchedrecipe.route('/searchedingredient/<recipe_id>', methods=["POST"])
-@login_required
-def add_ingredient(recipe_id):
-    payload = request.get_json()
-    print(type(payload), 'ingredient payload')
-    add_ingredient_recipe_id = recipe_id
-    # with g.db.atomic():
-    for x in range(len(payload)): 
-        add_ingredient = models.SearchedIngredients.create(ingredient=[payload["ingredient"]], recipe=add_ingredient_recipe_id)
-        new_ingredient = model_to_dict(add_ingredient)
-        return jsonify(data=new_ingredient, status={"code": 200, "message": "Successfully added ingredient"})
+# @searchedrecipe.route('/searchedingredient/<recipe_id>', methods=["POST"])
+# @login_required
+# def add_ingredient(recipe_id):
+#     payload = request.get_json()
+#     print(type(payload), 'ingredient payload')
+#     add_ingredient_recipe_id = recipe_id
+#     # with g.db.atomic():
+#     for x in range(len(payload)): 
+#         add_ingredient = models.SearchedIngredients.create(ingredient=[payload["ingredient"]], recipe=add_ingredient_recipe_id)
+#         new_ingredient = model_to_dict(add_ingredient)
+#         return jsonify(data=new_ingredient, status={"code": 200, "message": "Successfully added ingredient"})
 
 @searchedrecipe.route('/<id>', methods=["DELETE"])
 @login_required
@@ -52,11 +52,11 @@ def delete_recipe(id):
     status={"code": 200}
     )
 
-@searchedrecipe.route('/delete-all-ingredients/<recipe_id>', methods=["DELETE"])
-@login_required
-def delete_all_ingredients(recipe_id):
-    # find_recipe_id = [recipe for recipe in models.Ingredients.recipe if recipe["id"]==recipe_id]
-    delete_all_ingredients_query= models.SearchedIngredients.delete().where(models.SearchedIngredients.recipe==recipe_id)
-    num_of_rows_ingredient_deleted = delete_all_ingredients_query.execute()
-    print(num_of_rows_ingredient_deleted)
-    return jsonify(data={}, message="Successfully deleted {} ingredients with id {}".format(num_of_rows_ingredient_deleted, recipe_id), status={"code":200}) 
+# @searchedrecipe.route('/delete-all-ingredients/<recipe_id>', methods=["DELETE"])
+# @login_required
+# def delete_all_ingredients(recipe_id):
+#     # find_recipe_id = [recipe for recipe in models.Ingredients.recipe if recipe["id"]==recipe_id]
+#     delete_all_ingredients_query= models.SearchedIngredients.delete().where(models.SearchedIngredients.recipe==recipe_id)
+#     num_of_rows_ingredient_deleted = delete_all_ingredients_query.execute()
+#     print(num_of_rows_ingredient_deleted)
+#     return jsonify(data={}, message="Successfully deleted {} ingredients with id {}".format(num_of_rows_ingredient_deleted, recipe_id), status={"code":200}) 
